a66fae6dbc69b7ec70f98a9de0afa47f500eef72
mach-port-t <fridakitten@pm.me>
2026-08-17 14:05:08 +0200

    refactor: only validate when build type is running target

--- Nyxian/LindChain/IDEBuilder/NXBuilder.swift
+++ Nyxian/LindChain/IDEBuilder/NXBuilder.swift
@@ -284,19 +284,19 @@ class NXBuilder: NSObject, MDKDriverDelegate, MDKPhaseRunnerDelegate {
             XCButton.stopSpinning()
         }
         
-        var success: Bool = false;
-        let semaphore = DispatchSemaphore(value: 0)
-        checkSigningSetup() { reply in
-            success = reply
-            semaphore.signal()
-        }
-        semaphore.wait()
-        
-        if !success {
-            throw NSError(domain: "com.cr4zy.nyxian.builder.install", code: 1, userInfo: [NSLocalizedDescriptionKey:"Code signing is not properly set up. Cannot sign targets."])
-        }
-        
         if(buildType == .RunningApp) {
+            var success: Bool = false;
+            let semaphore = DispatchSemaphore(value: 0)
+            checkSigningSetup() { reply in
+                success = reply
+                semaphore.signal()
+            }
+            semaphore.wait()
+            
+            if !success {
+                throw NSError(domain: "com.cr4zy.nyxian.builder.install", code: 1, userInfo: [NSLocalizedDescriptionKey:"Code signing is not properly set up. Cannot sign targets."])
+            }
+            
             if self.project.projectConfig.schemeKind == .app {
                 let semaphore = DispatchSemaphore(value: 0)
                 var nsError: NSError? = nil
