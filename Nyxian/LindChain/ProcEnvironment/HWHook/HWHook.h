a26e72008d39c6c82f54ac2ca417b785a0ce1f80
mach-port-t <fridakitten@pm.me>
2026-08-20 09:38:14 +0200

    feat: added HWHook framework PoC

---
+++ Nyxian/LindChain/ProcEnvironment/HWHook/HWHook.h
@@ -0,0 +1,36 @@
+/*
+ SPDX-License-Identifier: AGPL-3.0-or-later
+
+ Copyright (C) 2025 - 2026 emexlab
+
+ This file is part of Nyxian.
+
+ Nyxian is free software: you can redistribute it and/or modify
+ it under the terms of the GNU Affero General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ Nyxian is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ GNU Affero General Public License for more details.
+
+ You should have received a copy of the GNU Affero General Public License
+ along with Nyxian. If not, see <https://www.gnu.org/licenses/>.
+*/
+
+#ifndef HWHOOK_H
+#define HWHOOK_H
+
+#include <CoreFoundation/CoreFoundation.h>
+
+typedef struct __HWHook *HWHookRef;
+
+CF_EXPORT CFTypeID HWHookGetTypeID(void);
+
+CF_EXPORT HWHookRef HWHookCreateWithPointerToSymbol(CFAllocatorRef allocator, void *symbol, void *replacement);
+
+CF_EXPORT void *HWHookGetSymbolPtr(HWHookRef hook);
+CF_EXPORT void *HWHookGetReplacementPtr(HWHookRef hook);
+
+#endif /* HWHOOK_H */
