ae900d62af954ad07a02c4913ba268a0f86d2950
mach-port-t <fridakitten@pm.me>
2026-08-23 15:39:44 +0200

    refactor: increased hit surface on window buttons

---
+++ Nyxian/LindChain/WindowServer/Window/NXHitSurfaceButton.h
@@ -0,0 +1,32 @@
+/*
+ SPDX-License-Identifier: AGPL-3.0-or-later
+
+ Copyright (C) 2023 - 2025 LiveContainer
+ Copyright (C) 2025 - 2026 emexlab
+
+ This file is part of LiveContainer.
+
+ LiveContainer is free software: you can redistribute it and/or modify
+ it under the terms of the GNU Affero General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ LiveContainer is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ GNU Affero General Public License for more details.
+
+ You should have received a copy of the GNU Affero General Public License
+ along with Nyxian. If not, see <https://www.gnu.org/licenses/>.
+*/
+
+#ifndef NXHITSURFACEBUTTON_H
+#define NXHITSURFACEBUTTON_H
+
+#import <UIKit/UIKit.h>
+
+@interface NXHitSurfaceButton : UIButton
+@property (nonatomic) UIEdgeInsets hitSurface;
+@end
+
+#endif /* NXHITSURFACEBUTTON_H */
