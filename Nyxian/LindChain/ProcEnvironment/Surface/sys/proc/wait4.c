e32906c1499eb2f49d0a85836bc82e7949960279
mach-port-t <fridakitten@pm.me>
2026-08-25 04:09:15 +0200

    security: patched memory corruption vulnerability in kvobject_event_trigger and wait4_proc_event_handler
    back then reentrance was needed into the kvobject_event_trigger, but nowadays I don't know any kind of operation that truly needs handler reentrance, no handler triggers a event on it's own task

--- Nyxian/LindChain/ProcEnvironment/Surface/sys/proc/wait4.c
+++ Nyxian/LindChain/ProcEnvironment/Surface/sys/proc/wait4.c
@@ -41,10 +41,15 @@ bool wait4_proc_event_handler(uint32_t type,
     wait4_payload_t *payload = (wait4_payload_t*)(event->ctx);
     ksurface_proc_t *child = (ksurface_proc_t*)(uintptr_t)val;
     
-    if(child == NULL)
+    if(type == kvObjEventUnregister)
     {
         mach_port_deallocate(mach_task_self(), payload->task);
         free(payload);
+        return true;
+    }
+    
+    if(child == NULL)
+    {
         return true;
     }
     
