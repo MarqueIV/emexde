2c5eb59c947e903bcd1c09b04a980ed2a6c7adb7
mach-port-t <fridakitten@pm.me>
2026-08-26 05:31:36 +0200

    security: making the trustDaemonPath registry tighter

--- Nyxian/LindChain/ProcEnvironment/Surface/trust/trust.m
+++ Nyxian/LindChain/ProcEnvironment/Surface/trust/trust.m
@@ -39,12 +39,12 @@
 #import <ksurface_config.h>
 
 /* ----------------------------------------------------------------------
- *  Constants
+ *  Types
  * -------------------------------------------------------------------- */
-const char *trustDaemonPath[] = {   /* those paths are immutable */
-    "/sbin/launchd",
-    "/usr/libexec/bootstrapd",
-};
+typedef struct {
+    const char *path;
+    CFDictionaryRef entitlementPreset;
+} trustDaemonEntry;
 
 /* ----------------------------------------------------------------------
  *  Functions
@@ -420,9 +420,16 @@ ksurface_trust_identity_t *trust_identity_create_from_path(const char *path)
     }
     
     /* daemon trustpath validation */
-    for(int index = 0; index < sizeof(trustDaemonPath) / sizeof(const char*); index++)
+    const trustDaemonEntry trustDaemonPath[] = {   /* those paths are immutable */
+        {
+            .path = "/usr/libexec/bootstrapd",
+            .entitlementPreset = kPEEntitlementsNXT2PresetsDaemonBootstrap,
+        }
+    };
+    
+    for(int index = 0; index < sizeof(trustDaemonPath) / sizeof(trustDaemonEntry); index++)
     {
-        if(strncmp(path, trustDaemonPath[index], MAXPATHLEN - 1) == 0)
+        if(strncmp(path, trustDaemonPath[index].path, MAXPATHLEN - 1) == 0)
         {
             ksurface_trust_identity_t *identity = calloc(1, sizeof(ksurface_trust_identity_t));
             if(identity == NULL)
@@ -440,14 +447,14 @@ ksurface_trust_identity_t *trust_identity_create_from_path(const char *path)
             }
             strlcpy(identity->path, path, MAXPATHLEN);
             identity->trustLevel = kPETrustLevelTrusted;
-            identity->entitlements = CFDictionaryCreateCopy(kCFAllocatorDefault, kPEEntitlementsNXT2PresetsDaemon);
+            identity->entitlements = CFDictionaryCreateCopy(kCFAllocatorDefault, trustDaemonPath[index].entitlementPreset);
             if(identity->entitlements == NULL)
             {
                 errno = ENOMEM;
                 CFRelease(executableString);
                 return NULL;
             }
-            identity->legacyEntitlements = trust_identity_legacy_entitlements_from_entitlements(kPEEntitlementsNXT2PresetsDaemon);
+            identity->legacyEntitlements = trust_identity_legacy_entitlements_from_entitlements(kPEEntitlementsNXT2PresetsDaemonBootstrap);
             identity->legacyEntitlements = identity->legacyEntitlements;
             identity->filePermissions = trust_identity_give_file_permissions(executableString, identity->entitlements);
             return identity;
