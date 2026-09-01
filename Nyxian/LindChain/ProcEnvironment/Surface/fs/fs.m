80f21a7de1a719ee538235c7e5a7e8e98f6c60b6
mach-port-t <fridakitten@pm.me>
2026-09-02 00:36:46 +0200

    refactor: creating klog_old in devfs

--- Nyxian/LindChain/ProcEnvironment/Surface/fs/fs.m
+++ Nyxian/LindChain/ProcEnvironment/Surface/fs/fs.m
@@ -77,7 +77,7 @@ kern_return_t ksurface_fs_init(void)
             [[NSString stringWithFormat:@"%s/Documents/rootfs", home] UTF8String],
         },
         {
-            kFSMountAttrRead | kFSMountAttrClear,
+            kFSMountAttrRead,
             [[NSString stringWithFormat:@"%s/Documents/mntfs/devfs", home] UTF8String],
         },
         {
