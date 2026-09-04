fdc25acc2bdd7a24b1d20c37cf7cee3f1f1a2ee0
mach-port-t <fridakitten@pm.me>
2026-09-05 01:35:02 +0200

    feat: using kvobj API on the kexts for natural dependency tracking

---
+++ Nyxian/LindChain/ProcEnvironment/Surface/kxld/image.c
@@ -0,0 +1,80 @@
+/*
+ SPDX-License-Identifier: AGPL-3.0-or-later
+
+ Copyright (C) 2025 - 2026 emexlab
+
+ This file is part of Nyxian.
+
+ Nyxian is free software: you can redistribute it and/or modify
+ it under the terms of the GNU Affero General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ Nyxian is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ GNU Affero General Public License for more details.
+
+ You should have received a copy of the GNU Affero General Public License
+ along with Nyxian. If not, see <https://www.gnu.org/licenses/>.
+*/
+
+#include <LindChain/ProcEnvironment/Surface/kxld/image.h>
+#include <LindChain/ProcEnvironment/Surface/kxld/resolve.h>
+#include <LindChain/ProcEnvironment/Utils/klog.h>
+#include <LindChain/ProcEnvironment/Utils/kpanic.h>
+#include <os/lock.h>
+
+DEFINE_KVOBJECT_MAIN_EVENT_HANDLER(kxld_image)
+{
+    /* handle size request */
+    if(kvarr == NULL)
+    {
+        return (int64_t)sizeof(kxld_image_info_t);
+    }
+    
+    /* get our kobj */
+    kxld_image_info_t *image_info = (kxld_image_info_t*)kvarr[0];
+    
+    switch(type)
+    {
+        case kvObjEventCopy:
+        case kvObjEventSnapshot:
+            ksurface_panic("attempting to copy or snapshot a kxld image object is illegal");
+        case kvObjEventDeinit:
+            if(!(image_info->mod->flags & KMOD_FLAG_PERSISTENT))
+            {
+                if(image_info->isStarted && image_info->mod->start)
+                {
+                    kern_return_t kr = image_info->mod->stop();
+                    if(kr != KERN_SUCCESS)
+                    {
+                        ksurface_panic("kext '%s' failed to stop: %s", image_info->mod->identifier, mach_error_string(kr));
+                    }
+                }
+                klog_log("kextloader:unload", "deinitilizing kext '%s'", image_info->mod->identifier);
+                if(image_info->isInitialized && image_info->mod->deinit)
+                {
+                    kern_return_t kr = image_info->mod->deinit();
+                    if(kr != KERN_SUCCESS)
+                    {
+                        ksurface_panic("kext '%s' failed to deinitialize: %s", image_info->mod->identifier, mach_error_string(kr));
+                    }
+                }
+                kern_return_t kr = KXUnregisterKext(image_info);
+                if(kr == KERN_SUCCESS || kr == KERN_NOT_FOUND)
+                {
+                    if(image_info->base != NULL && image_info->safeToUnmap)
+                    {
+                        munmap(image_info->base, image_info->len);
+                    }
+                }
+                else
+                {
+                    ksurface_panic("kext '%s' failed to unregister: %s", image_info->mod->identifier, mach_error_string(kr));
+                }
+            }
+        default:
+            return 0;
+    }
+}
