39a7dd574647581f96d7dbe3c8480c801a89eb55
mach-port-t <fridakitten@pm.me>
2026-09-06 13:38:38 +0200

    feat: backup and restore app resizing

--- Nyxian/LindChain/WindowServer/Window/NXWindow.m
+++ Nyxian/LindChain/WindowServer/Window/NXWindow.m
@@ -367,6 +367,7 @@
         case UIGestureRecognizerStateCancelled:
         case UIGestureRecognizerStateFailed:
             [self updateOriginalFrame];
+            [self.session windowDidMove];
         default:
             break;
     }
@@ -404,7 +405,7 @@ static inline CGSize EVSizeForAspect(CGSize base,
         case UIGestureRecognizerStateBegan:
             [self focusWindow];
             [gesture setTranslation:CGPointZero inView:self.view.superview];
-            [self.session beginInteractiveResize];
+            [self.session windowBeganResizing];
             [_windowBar collapseIsland];
             if(self.ratioLocked)
             {
@@ -480,7 +481,7 @@ static inline CGSize EVSizeForAspect(CGSize base,
         case UIGestureRecognizerStateCancelled:
         case UIGestureRecognizerStateFailed:
             [self updateOriginalFrame];
-            [self.session commitInteractiveResize];
+            [self.session windowEndedResizing];
             break;
         default:
             break;
